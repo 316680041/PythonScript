a55ea48ab76835452281a948ff1e56ca63d21156
jieguanglin <578010377@qq.com>
2017-10-13 23:39:56 +0800

    修改无法破解的问题，外加显示破解进度。

--- cryptPass/cryptPass.py
+++ cryptPass/cryptPass.py
@@ -3,21 +3,22 @@ from crypt import crypt
 
 def testPass(dictPath,cryptSalt,cryptPass):
 	dictFile = open(dictPath, 'r') #打开字典文件
-	while True:
-		block = dictFile.read(1024)#获取字典大小1024字节
-		if not block:
-			print('Password not found !')#匹配失败，返回
+	# while True:
+	# 	block = dictFile.read(1024)#获取字典大小1024字节
+	# 	if not block:
+	# 		print('Password not found !')#匹配失败，返回
+	# 		return
+	for word in dictFile:#读取字典
+		word = word.strip('\n') #保留原始的字符，不去空格
+		print('Current progress is',word)
+		cryptWord = crypt(word, cryptSalt)#获取字典的密码和盐结合
+		if cryptPass == cryptWord:#匹配密码
+			print('passed is : ', word)#匹配成功输出解密后的密码，然后返回
 			return
-		for word in block:#读取字典
-			word = word.strip('\n') #保留原始的字符，不去空格
-			cryptWord = crypt(word, cryptSalt)#获取字典的密码和盐结合
-			if cryptPass == cryptWord:#匹配密码
-				print('passed is : ', word)#匹配成功输出解密后的密码，然后返回
-				return
 
 def main():
-	passPath = raw_input('请输入密码文件路径')
-	dictPath = raw_input('请输入字典文件路径')
+	passPath = input('请输入密码文件路径')
+	dictPath = input('请输入字典文件路径')
 	passFile = open(passPath, 'r')#打开密码文件
 	for line in passFile.readlines():
 		user = line.split(':')[0]#获取用户名
